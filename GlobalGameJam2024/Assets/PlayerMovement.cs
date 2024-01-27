ddaee80236cbf9d35c261e8f54bcde74f13a6fc6
AnishApple <eonisdevil@gmail.com>
2024-01-27 14:26:25 -0800

    movement stuffs

---
+++ GlobalGameJam2024/Assets/PlayerMovement.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float playerSpeed
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
