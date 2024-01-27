9e01fc4771c3409078f26a7a2596a1173a3ea612
AnishApple <eonisdevil@gmail.com>
2024-01-27 14:48:01 -0800

    enemy follows player

--- GlobalGameJam2024/Assets/CarrotFollow.cs
+++ GlobalGameJam2024/Assets/CarrotFollow.cs
@@ -1,17 +1,36 @@
+using System.Numerics;
 using UnityEngine;
 
-public class CarrotFollow : MonoBehaviour
+public class TestEnemy : Enemy
 {
-    public float enemySpeed;
-    // Start is called before the first frame update
-    void Start()
+    // Note to whoever's editing this code: you don't need to add a health variable here because this class inherits from Enemy which already has health functionality.
+    [SerializeField] private int speed = 1;
+    [SerializeField] private float distanceFromPlayer = 5f;
+    private GameObject player;
+    private Player playerScript;
+    private Rigidbody testEnemyRb;
+    private float enemySpeed;
+
+    private void Awake()
     {
-        
+        health = maxHealth; // have to include this line to set health in any enemy class
+        testEnemyRb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (Vector3.Distance(transform.position, player.transform.position) > distanceFromPlayer)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, player.transform.position, speed * Time.deltaTime);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject == player)
+        {
+            playerScript.ChangeHealth(-1);
+        }
     }
 }
