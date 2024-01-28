dbe99c3402db1fb7e23abe43401155aab47c482e
AnishApple <eonisdevil@gmail.com>
2024-01-27 16:00:26 -0800

    scoring

--- GlobalGameJam2024/Assets/CarrotFollow.cs
+++ GlobalGameJam2024/Assets/CarrotFollow.cs
@@ -3,17 +3,30 @@ using Vector3 = UnityEngine.Vector3;
 
 public class CarrotFollow : MonoBehaviour
 {
+    public float score;
+    public float initialTime;
     [SerializeField] private GameObject player;
     [SerializeField] private int enemySpeed = 2;
     [SerializeField] private float distanceFromPlayer = 2f;
     private float _enemySpeed;
 
+    void Start()
+    {
+        initialTime = Time.time;
+    }
+
     // Update is called once per frame
     private void Update()
     {
+
         if (Vector3.Distance(transform.position, player.transform.position) > distanceFromPlayer)
         {
             transform.position = Vector3.MoveTowards(transform.position, player.transform.position, enemySpeed * Time.deltaTime);
+
+        }
+        else
+        {
+            score = Time.time - initialTime;
         }
     }
 
